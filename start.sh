2e99566ace86ab75e83adb0a991796eb8e4a7cc0
Jarno Nevala <nevala.jarno@gmail.com>
2024-08-10 12:04:05 +0700

    build minor change

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ ARC=$(uname -m)
 
 if [ "$ARC" = "aarch64" ]; then
     if [ ! -f "home_be_arm64" ]; then
+        /usr/bin/make clean 
         /usr/bin/go mod init github.com/janevala/home_be
         /usr/bin/go mod tidy
         /usr/bin/go get github.com/mmcdole/gofeed
@@ -18,6 +19,7 @@ fi
 
 if [ "$ARC" = "x86_64" ]; then
     if [ ! -f "home_be_amd64" ]; then
+        /usr/bin/make clean 
         /usr/bin/go mod init github.com/janevala/home_be
         /usr/bin/go mod tidy
         /usr/bin/go get github.com/mmcdole/gofeed
