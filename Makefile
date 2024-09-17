0d968ccbf5573e273a4704854eeca25beff2fe68
Jarno Nevala <nevala.jarno@gmail.com>
2024-09-17 11:54:23 +0700

    build optimizations

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 BINARY_NAME=home_be
-
+GOOS=linux
 BUILDARCH ?= $(shell uname -m)
 
 ifeq ($(BUILDARCH),aarch64)
@@ -10,7 +10,7 @@ ifeq ($(BUILDARCH),x86_64)
 endif
 
 help:
-	@echo "make dep - Install dependencies"
+	@echo "make dep - Install dependencies for compiling the binary file"
 	@echo "make build - Build the binary file"
 	@echo "make run - Run the binary file"
 	@echo "make build_and_run - Build and run the binary file"
@@ -20,15 +20,19 @@ dep:
 	go mod tidy && go mod vendor && go fmt
 
 build:
-	GOOS=linux GOARCH=${BUILDARCH} go build -o ${BINARY_NAME}_${BUILDARCH} main.go
+	go mod init github.com/janevala/home_be
+	go mod tidy
+	go get github.com/mmcdole/gofeed
+	go get github.com/google/uuid
+	go get github.com/lib/pq
+	GOARCH=${BUILDARCH} go build -o ${BINARY_NAME}_${BUILDARCH} main.go
 
 run:
 	./${BINARY_NAME}_${BUILDARCH}
 
-build_and_run:
-	GOOS=linux GOARCH=${BUILDARCH} go build -o ${BINARY_NAME}_${BUILDARCH} main.go && ./${BINARY_NAME}_${BUILDARCH}
-
 clean:
 	go clean
 	rm -rf vendor
+	rm -rf go.sum
+	rm -rf go.mod
 	rm -f ${BINARY_NAME}_${BUILDARCH}
--- start.sh
+++ start.sh
@@ -4,14 +4,10 @@ ARC=$(uname -m)
 
 if [ "$ARC" = "aarch64" ]; then
     if [ ! -f "home_be_arm64" ]; then
-        /usr/bin/make clean 
-        /usr/bin/go mod init github.com/janevala/home_be
-        /usr/bin/go mod tidy
-        /usr/bin/go get github.com/mmcdole/gofeed
-        /usr/bin/go get github.com/google/uuid
-        /usr/bin/go get github.com/lib/pq
-
-        /usr/bin/make build_and_run
+        /usr/bin/make clean
+        /usr/bin/make build
+        /usr/bin/make dep
+        /usr/bin/make run
     else
         ./home_be_arm64
     fi
@@ -19,14 +15,10 @@ fi
 
 if [ "$ARC" = "x86_64" ]; then
     if [ ! -f "home_be_amd64" ]; then
-        /usr/bin/make clean 
-        /usr/bin/go mod init github.com/janevala/home_be
-        /usr/bin/go mod tidy
-        /usr/bin/go get github.com/mmcdole/gofeed
-        /usr/bin/go get github.com/google/uuid
-        /usr/bin/go get github.com/lib/pq
-
-        /usr/bin/make build_and_run
+        /usr/bin/make clean
+        /usr/bin/make build
+        /usr/bin/make dep
+        /usr/bin/make run
     else
         ./home_be_amd64
     fi
