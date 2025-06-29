2492cb86cf6e4f89b3e49e300a3a6075c92487d5
Jarno Nevala <jarno.nevala@codemate.com>
2025-06-29 21:00:17 +0700

    small

--- llog/release.go
+++ llog/release.go
@@ -2,14 +2,31 @@
 
 package llog
 
+import "log"
+
 func Out(v ...any) {
 }
 
 func Err(err error) {
+	if err == nil {
+		return
+	}
+
+	log.Printf("[ERROR] %s", err.Error())
 }
 
 func Fatal(v ...any) {
+	if len(v) == 0 {
+		return
+	}
+
+	log.Fatalf("[FATAL] %v", v...)
 }
 
 func Fatalf(format string, args ...interface{}) {
+	if format == "" {
+		return
+	}
+
+	log.Fatalf("[FATAL] "+format, args...)
 }
